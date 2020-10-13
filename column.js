4425675ca513e40c8c634eab8fd989a1fbfad969
jadon-paulse <jadonpaulse@gmail.com>
2020-10-13 09:24:05 +0200

    Finished column

---
+++ column.js
@@ -0,0 +1,16 @@
+var n = -5;
+var i;
+
+
+for (i = n; i < n + 41; i += 7){
+        
+        
+    if(-6 < n < 2){
+        //var j = "Steve";
+        console.log(i);
+        //document.write("Steve");
+    }
+    else{
+        console.log("Error");
+    }
+};
